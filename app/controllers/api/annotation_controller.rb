9a705c51964c6f9faef36b67797765664340570e
René Sprotte <rs@metaminded.com>
2012-01-19 14:08:59 +0100

    API to delete annotations

--- app/controllers/api/annotation_controller.rb
+++ app/controllers/api/annotation_controller.rb
@@ -1,18 +1,29 @@
 class Api::AnnotationController < ApplicationController
 
+  before_filter :setup_semapp_and_epub
+
   def create
-    semapp = Semapp.find(params[:semapp_id])
-    epub   = semapp.epubs.find(params[:epub_id])
-    epub.annotations << Annotation.new(data: params[:data])
+    @epub.annotations << Annotation.new(data: params[:data])
     render nothing: true
   end
 
   def update
-    semapp     = Semapp.find(params[:semapp_id])
-    epub       = semapp.epubs.find(params[:epub_id])
-    annotation = epub.annotations.find(params[:id])
+    annotation = @epub.annotations.find(params[:id])
     annotation.update_attribute(:data, params[:data])
     render nothing: true
+  end
+
+  def destroy
+    annotation = @epub.annotations.find(params[:id])
+    annotation.destroy
+    render nothing: true
+  end
+
+  private
+
+  def setup_semapp_and_epub
+    @semapp = Semapp.find(params[:semapp_id])
+    @epub   = @semapp.epubs.find(params[:epub_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Epubdummy::Application.routes.draw do
 
   namespace :api do
-    get 'semapps'                      => 'semapp#index'
-    get 'semapps/:semapp_id'           => 'semapp#show'
+    get 'semapps'            => 'semapp#index'
+    get 'semapps/:semapp_id' => 'semapp#show'
 
-    post 'semapps/:semapp_id/epubs/:epub_id/annotations'     => 'annotation#create'
-    post 'semapps/:semapp_id/epubs/:epub_id/annotations/:id' => 'annotation#update'
+    post   'semapps/:semapp_id/epubs/:epub_id/annotations'     => 'annotation#create'
+    put    'semapps/:semapp_id/epubs/:epub_id/annotations/:id' => 'annotation#update'
+    delete 'semapps/:semapp_id/epubs/:epub_id/annotations/:id' => 'annotation#destroy'
   end
 
 end
